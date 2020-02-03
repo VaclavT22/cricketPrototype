49937cf8e20a510e0f6823dfa294734540c3cd6e
Vaclav Taska <v.taska@seznam.cz>
2020-02-03 22:20:49 +0100

    Initial commit for Product branch. Added methods for searching through products.
    src/main/java/com/taska/java/crickets/cricketfarm/controller/ProductController.java
    src/main/java/com/taska/java/crickets/cricketfarm/repository/ProductRepository.java

--- src/main/java/com/taska/java/crickets/cricketfarm/controller/ProductController.java
+++ src/main/java/com/taska/java/crickets/cricketfarm/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.taska.java.crickets.cricketfarm.controller;
 
+import java.math.BigDecimal;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.taska.java.crickets.cricketfarm.model.Product;
@@ -53,5 +55,30 @@ public class ProductController {
 	@DeleteMapping("/{id}")
 	public void deleteProduct(@PathVariable("id") Long id) {
 		productRepository.deleteById(id);
+	}
+	
+	@GetMapping("/likeName")
+	public List<Product> getProductsLikeName(@RequestParam(name = "productName") String productName) {
+		return productRepository.findByNameIgnoreCaseContaining(productName);
+	}
+	
+	@GetMapping("/likeAge")
+	public List<Product> getProductsLikeAge(@RequestParam String productAge) {
+		return productRepository.findByAgeIgnoreCaseContaining(productAge);
+	}
+	
+	@GetMapping("/likeVolume")
+	public List<Product> getProductsLikeVolume(@RequestParam Float productVolume) {
+		return productRepository.findByVolumeContaining(productVolume);
+	}
+	
+	@GetMapping("/likePackaging")
+	public List<Product> getProductsLikePackaging(@RequestParam String productPackaging){
+		return productRepository.findByPackagingIgnoreCaseContaining(productPackaging);
+	}
+	
+	@GetMapping("/likePrice") 
+	public List<Product> getProductsLikePrice(@RequestParam BigDecimal productPrice) {
+		return productRepository.findByPriceContaining(productPrice);
 	}
 }
--- src/main/java/com/taska/java/crickets/cricketfarm/repository/ProductRepository.java
+++ src/main/java/com/taska/java/crickets/cricketfarm/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.taska.java.crickets.cricketfarm.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.taska.java.crickets.cricketfarm.model.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	List<Product> findByNameIgnoreCaseContaining(String productName);
+	List<Product> findByAgeIgnoreCaseContaining(String productAge);
+	List<Product> findByVolumeContaining(Float productVolume);
+	List<Product> findByPackagingIgnoreCaseContaining(String productPackaging);
+	List<Product> findByPriceContaining(BigDecimal productPrice);
 }
